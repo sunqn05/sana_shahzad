1775b1c15b1254818a37bd95009c96f524477d82
Sana Shahzad <sana.shahzad@mail.utoronto.ca>
2026-05-20 22:56:31 -0400

    fixed netlify build

--- src/components/Carditem.js
+++ src/components/Carditem.js
@@ -6,16 +6,24 @@ function Carditem(props) {
     <>
       <li className='cards__item'>
         <Link className='cards__item__link' to={props.path}>
-          <figure className='cards__item__pic-wrap' data-category={props.label}>
+
+          <figure
+            className='cards__item__pic-wrap'
+            data-category={props.label}
+          >
             <img
               className='cards__item__img'
-              alt='Travel Image'
               src={props.src}
+              alt={props.label}
             />
           </figure>
+
           <div className='cards__item__info'>
-            <h5 className='cards__item__text'>{props.text}</h5>
+            <h5 className='cards__item__text'>
+              {props.text}
+            </h5>
           </div>
+
         </Link>
       </li>
     </>
