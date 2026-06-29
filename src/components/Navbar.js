75eb06b0ada43f445510e0d13fbc4c1fa76d2460
Sana Shahzad <sana.shahzad@mail.utoronto.ca>
2026-06-29 18:05:10 -0400

    f

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,12 +57,6 @@ function Navbar() {
                     Projects
                 </Link>
                 </li>
-
-                <li className='nav-item'>
-                <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
-                    About
-                </Link>
-                </li>
             </ul>
             {button && (
             <Button
